11385015ae8a98eb4b6c3de9b76e013c1473061e
Linisha-VL <linishavincent@gmail.com>
2018-07-09 12:58:05 +0530

    Linisha commit

---
+++ customername.py
@@ -0,0 +1,6 @@
+Name1=input("Enter the customer name")
+Name2=input("Enter the customer name")
+Name3=input("Enter the customer name")
+Name4=input("Enter the customer name")
+Name5=input("Enter the customer name")
+
